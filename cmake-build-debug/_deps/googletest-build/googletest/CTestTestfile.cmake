5f010a8eea423ae6b23f55e0ecc8c01c6659d768
dontwastemytimew <dalevska@icloud.com>
2025-10-31 21:55:48 +0200

    Fix: Delete .idea and update .gitignore

--- cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/clionprojects/project-oop-1-sem-2025/cmake-build-debug/_deps/googletest-src/googletest
-# Build directory: D:/clionprojects/project-oop-1-sem-2025/cmake-build-debug/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
