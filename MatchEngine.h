4aed84f644624a557eb9d8f5cf94f74a777cb137
Leyla <chevikleyla@gmail.com>
2025-11-02 22:49:17 +0200

    added MatchEngine.h

---
+++ MatchEngine.h
@@ -0,0 +1,34 @@
+#ifndef MATCHENGINE_H
+#define MATCHENGINE_H
+
+#include "Preference.h"
+#include <QString>
+
+/**
+ * @brief UserProfile struct
+ *
+ * структура для демонстрацї профілю користувача
+ */
+struct UserProfile {
+    QString name;
+    int age;
+    QString city;
+};
+
+/**
+ * @brief MatchEngine class
+ *
+ * клас для простого алгоритму фільтрування профілів за Preference
+ */
+class MatchEngine {
+public:
+    /**
+     * @brief calculateMatch
+     * @param profile Профіль користувача
+     * @param prefs Критерії пошуку
+     * @return true якщо профіль відповідає хоча б одному критерію
+     */
+    static bool calculateMatch(const UserProfile &profile, const Preference &prefs);
+};
+
+#endif // MATCHENGINE_H
